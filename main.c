8bc6d6c647a085b352ea063e58b95dcf08c84686
Christopher Bilger <christopherbilg@gmail.com>
2019-04-20 15:19:05 -0400

    Working on the initial structure, getting my head around it

--- main.c
+++ main.c
@@ -5,3 +5,11 @@
 #include "main.h"
 
 // Constants
+#define VIRTUAL_DISK_FILE "vDisk" // The name of the file on actual disk
+#define DISK_TOTAL_BLOCKS 20000 // can be changed
+#define FAT_SP 0 // starting point
+#define RESERVED_SP 1 // starting point
+#define FAT_DATA_SP 3 // starting point
+#define DISK_BLOCK_SIZE 512 // bytes
+#define MAXIMUM_ENTRY_SIZE 256 // blocks
+
--- main.h
+++ main.h
@@ -3,6 +3,20 @@
 
 #include <stdio.h>
 
+// 32-byte entries
+struct ENTRY {
+  char filename[23];
+  char file_extension[3];
+  char file_size[6];
+};
 
+// file allocation table (32-byte entries)
+struct FAT {
+  char reserved[6];
+  char data[6];
+  char next_block[6];
+  char valid[1]; // Validity byte shown in class
+  char filename[13];
+};
 
 #endif
